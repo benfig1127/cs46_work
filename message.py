428d274006af7e7ec49bc333e14bd3303a92f8e7
benfig1127 <benjifig@gmail.com>
2021-01-01 23:18:43 -0800

    added the first code

---
+++ message.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+def main(message):
+    print(f'{message} world!')
+
+if __name__=='__main__':
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--message',choices=['hello','goodbye'])
+    args = parser.parse_args()
+    main(args.message)
